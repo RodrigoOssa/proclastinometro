a31866e9a6ecd2bf0dd1bad35132ebacac8e3ba2
Rodrigo <rodrigo.o.2012.r@gmail.com>
2025-01-10 17:48:06 -0300

    Pantalla de login. Dashboard prueba. Completar el dashboard para ver todos los proyectos

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,23 @@
+import Navbar from "../ui/navbar";
+
+const Dashboard = () => {
+    return (
+        <div className="min-h-full">
+            <Navbar></Navbar>
+
+            <header className="bg-slate-500 shadow">
+                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h1 className="text-3xl font-bold tracking-tight text-gray-300">Dashboard</h1>
+                </div>
+            </header>
+            <main>
+                <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+                    <h3>¿Qué es Lorem Ipsum?</h3>
+                    <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas "Letraset", las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.</p>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ src/app/ui/footer.tsx
@@ -0,0 +1,12 @@
+export default function Footer() {
+    return (
+        <footer className="bg-gray-800 text-white py-2">
+            <div className="container mx-auto text-right">
+                <div></div>
+                <div>
+                    <p>Developed by Rodrah</p>
+                </div>
+            </div>
+        </footer>
+    )
+}
