2b06169d4542d6bf751080d186307787e23fab5f
Rodrigo <rodrigo.o.2012.r@gmail.com>
2025-01-17 11:07:03 -0300

    Terminando componente de ProjectList

--- src/components/ProjectsList.tsx
+++ src/components/ProjectsList.tsx
@@ -5,25 +5,25 @@ export default function ProjectList() {
     const projects = [
         {
             id: 1,
-            name: 'Project One',
+            name: 'Proyecto KFC',
             link: '/project-one',
             img: `/assets/img/1`
         },
         {
             id: 2,
-            name: 'Project Two',
+            name: 'Proyecto McDonalds',
             link: '/project-two',
             img: `/assets/img/2`
         },
         {
             id: 3,
-            name: 'Project Three',
+            name: 'Proyecto Burger King',
             link: '/project-three',
             img: `/assets/img/3`
         },
     ];
     return (
-        <section className="mx-auto px-4 py-6 max-w-7xl sm:px-6 lg:px-8">
+        <section className="mx-auto mb-10 px-4 py-6 max-w-7xl sm:px-6 lg:px-8">
             <h1 className="text-hColor dark:text-hColor text-2xl">
                 Proyectos
             </h1>
@@ -31,8 +31,8 @@ export default function ProjectList() {
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 mt-6">
                 {projects.map(project => (
                     <Link href={project.link} key={project.id} className="block p-6  rounded-lg  hover:border hover:border-gray-200 shadow-md hover:bg-gray-100 transform transition-transform duration-300 hover:scale-105 shadow-lg bg-white dark:bg-gray-800">
-                        <div className="project-card-content flex items-center justify-center" style={{ backgroundImage: `url('${project.img}/image-${project.id}.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center', color: 'white', height: '200px' }}>
-                            <h3 className="text-xl font-semibold text-orange-400">{project.name}</h3>
+                        <div className="project-card-content flex items-center justify-center tooltip" style={{ backgroundImage: `url('${project.img}/image-${project.id}.jpg')`, backgroundSize: 'cover', backgroundPosition: 'center', color: 'white', height: '200px' }}>
+                            <span className="text-xl font-semibold text-orange-400 tooltip-text bg-slate-700/75 dark:bg-slate-50/75">{project.name}</span>
                         </div>
                     </Link>
                 ))}
